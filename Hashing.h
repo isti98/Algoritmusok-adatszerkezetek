395dd08c1b4bc2ccbef062ab4c08cbbdd93abbce
isti98 <gwlxts@gmail.com>
2022-05-05 20:11:06 +0200

    hashing

---
+++ Hashing.h
@@ -0,0 +1,47 @@
+
+//#include""
+
+//HASÍTÓ TÁBLÁK ÉS HASÍTÓ FÜGVÉNYEK IMPLEMENTÁCIÓJA
+
+#ifndef HASHING_H
+#define HASHING_H
+
+//
+//
+//
+template<class T>
+class Hashable
+{
+	public:
+		unsigned int 		generateKey		(const T& e)const=0;
+	protected:
+		
+	
+};
+
+
+//
+//
+//
+template<class T>
+class HashTable
+{
+	public:
+		
+	protected:	
+
+};
+
+//
+//
+//
+template<class T>
+class Hashing
+{
+	public:
+	
+	protected:
+	
+};
+
+#endif
